97ff3e1bd162830daa0cb235b29e6614ba80e52c
ahnok0603 <nguyenthianhngoc060305@gmail.com>
2026-05-10 23:15:59 +0700

    update Calculator, add string vietnamese

--- app/src/main/java/com/example/k234111emobile/CalculatorActivity.java
+++ app/src/main/java/com/example/k234111emobile/CalculatorActivity.java
@@ -15,7 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class CalculatorActivity extends AppCompatActivity {
 
     Button buttoncal;
-    TextView txtMC, txtMR;
+    TextView txtMC, txtMR, txtM, txtMMINUS, txtMPLUS, txtMS;
+    View.OnClickListener click_m_listener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,18 +34,60 @@ public class CalculatorActivity extends AppCompatActivity {
     private void addEvent() {
         buttoncal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onClick(View view) {
+                processFormular();
             }
         });
+        click_m_listener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //process later
+                if(view.equals(txtM))
+                {
+
+                }
+                else if(view.equals(txtMC))
+                {
+
+                }
+                //không được if view ==txtMC vì nó đang so sánh ô nhớ trên RAM
+            }
+        };
+        txtM.setOnClickListener(click_m_listener);
+        txtMC.setOnClickListener(click_m_listener);
+        txtMMINUS.setOnClickListener(click_m_listener);
+        txtMR.setOnClickListener(click_m_listener);
+        txtMPLUS.setOnClickListener(click_m_listener);
+        txtMS.setOnClickListener(click_m_listener);
+    }
+
+    private void processFormular() {
+        //Step 1: get formular:
+        EditText edtFormular=findViewById(R.id.editFormular);
+        String formular=edtFormular.getText().toString();
+
+        //step 2: find api (lib) to calculate formular
+        String result="";
+        //result=....(formular)
+
+        //step 3: show result
+        edtFormular.setText(result);
     }
 
     private void addView() {
+        buttoncal=findViewById(R.id.buttoncal);
+        txtMC=findViewById(R.id.txtMC);
+        txtMR=findViewById(R.id.txtMR);
+        txtMPLUS=findViewById(R.id.txtMPLUS);
+        txtMMINUS=findViewById(R.id.txtMMINUS);
+        txtMS=findViewById(R.id.txtMS);
+        txtM=findViewById(R.id.txtM);
     }
 
     public void processChooseValue(View view) {
         //get current view clicked
         Button btn= (Button) view;
+        //get edtFormular
         EditText edtFormular=findViewById(R.id.editFormular);
         String old_value=edtFormular.getText().toString();
         String new_value=btn.getText().toString();
@@ -57,8 +100,9 @@ public class CalculatorActivity extends AppCompatActivity {
         String new_value="";
         if(old_value.length()>1)
         {
+            //remove the last character:
             new_value=old_value.substring(0,old_value.length()-1);
         }
-
+        edtFormular.setText(new_value);
     }
 }
